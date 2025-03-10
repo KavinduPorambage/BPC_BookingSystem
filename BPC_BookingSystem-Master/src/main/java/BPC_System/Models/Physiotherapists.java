8cbd982641726423a85f57e979808c323cf73a08
KavinduPorambage <kp24aax@herts.ac.uk>
2025-03-10 20:20:01 +0000

    Physiotherapists model

---
+++ BPC_BookingSystem-Master/src/main/java/BPC_System/Models/Physiotherapists.java
@@ -0,0 +1,60 @@
+package BPC_System.Models;
+
+import java.util.List;
+import java.util.Map;
+
+public class Physiotherapists {
+
+    private int id;
+    private String name;
+    private String address;
+    private String phone;
+    private Map<String, List<String>> expertiseArea;
+
+    public Physiotherapists() {
+    }
+
+    public Physiotherapists(int id, String name, String address, String phone, Map<String, List<String>> expertiseArea) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.expertiseArea = expertiseArea;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String fullName) {
+        this.name = fullName;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Map<String, List<String>> getExpertiseAreas() {
+        return this.expertiseArea;
+    }
+    public void setExpertiseAreas(Map<String, List<String>> expertiseAreas) {
+        this.expertiseArea = expertiseAreas;
+    }
+
+}
