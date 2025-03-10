7f63a801155356cf6bf93dfc8872d37537893bc0
KavinduPorambage <kp24aax@herts.ac.uk>
2025-03-10 20:17:06 +0000

    Patient model

--- BPC_BookingSystem-Master/src/main/java/BPC_System/Models/Patient.java
+++ BPC_BookingSystem-Master/src/main/java/BPC_System/Models/Patient.java
@@ -1,4 +1,45 @@
 package BPC_System.Models;
 
 public class Patient {
+
+    private int id;
+    private String Name;
+    private String address;
+    private String phone;
+
+    public Patient(int id, String name, String address, String phone) {
+        this.id = id;
+        this.Name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
 }
