ad9a62f73b90f4dca9ca970ce794d51837af485a
KavinduPorambage <kp24aax@herts.ac.uk>
2025-03-10 20:20:39 +0000

    Timetable Model

---
+++ BPC_BookingSystem-Master/src/main/java/BPC_System/Models/Timetable.java
@@ -0,0 +1,73 @@
+package BPC_System.Models;
+
+public class Timetable {
+
+    private int id;
+    private int physioId;
+    private int patientId;
+    private String status;
+    private String date;
+    private String time;
+    private String expertiseArea;
+    private String treatment;
+
+    public Timetable() {
+    }
+
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPhysioId() {
+        return this.physioId;
+    }
+    public void setPhysioId(int physioId) {
+        this.physioId = physioId;
+    }
+
+    public int getPatientId() {
+        return this.patientId;
+    }
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return this.time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getExpertiseArea() {
+        return this.expertiseArea;
+    }
+    public void setExpertiseArea(String expertiseArea) {
+        this.expertiseArea = expertiseArea;
+    }
+
+    public String getTreatment() {
+        return this.treatment;
+    }
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+}
