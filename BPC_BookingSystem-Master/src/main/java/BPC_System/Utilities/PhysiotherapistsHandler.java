8f38601890f598ed17b2dd357210e782e52dff5c
KavinduPorambage <kp24aax@herts.ac.uk>
2025-03-26 03:31:32 +0530

    complete PhysiotherapistsHandler

---
+++ BPC_BookingSystem-Master/src/main/java/BPC_System/Utilities/PhysiotherapistsHandler.java
@@ -0,0 +1,43 @@
+package BPC_System.Utilities;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import BPC_System.Models.PhysiotherapistsModel;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhysiotherapistsHandler {
+
+    // save
+    public static List<PhysiotherapistsModel> readPhysiosFromFile() {
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            return new ArrayList<>(); // Return empty list if file does not exist
+        }
+        try {
+            return objectMapper.readValue(file, new TypeReference<List<PhysiotherapistsModel>>() {});
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+    private static final String FILE_PATH = "src/main/java/BPC_System/Files/Physiotherapists.json";
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    // Save
+    public static boolean savePhysiosToJson(List<PhysiotherapistsModel> physios) {
+        try {
+            objectMapper.writeValue(new File(FILE_PATH), physios);
+            return true;
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+            return false;
+        }
+    }
+
+}
